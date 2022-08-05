2bffb53b9c9d2b7f100c927e5de30de0693cb5e5
zwellvvb <zwellvvb@gmail.com>
2022-08-05 09:11:42 +0800

    Auth install

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,12 +14,11 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
+        User::create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]);
-        $user->assignRole('Employee', 'Admin');
+        ])->assignRole('Employee', 'Admin');
     }
 }
